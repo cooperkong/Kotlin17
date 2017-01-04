5e44e51e1e0088862dd8e19c8346bd1f660f3ee6
WenChao Kong <wenchao.kong@gmail.com>
2017-01-04 14:02:56 +1100

    reformat

--- designpattern/src/bridgepattern/Circle.kt
+++ designpattern/src/bridgepattern/Circle.kt
@@ -3,12 +3,7 @@ package bridgepattern
 /**
  * Created by wenchaokong on 3/01/2017.
  */
-class Circle(x:Int, y:Int, radius:Int, drawAPI: DrawAPI) : Shape(drawAPI) {
-
-    val drawAPI: DrawAPI = drawAPI
-    val x = x
-    val y = y
-    val radius = radius
+class Circle(val x:Int, val y:Int, val radius:Int, val drawAPI: DrawAPI) : Shape(drawAPI) {
 
     override fun draw() {
         drawAPI.drawCircle(x, y, radius)
