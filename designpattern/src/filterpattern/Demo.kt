487f81f93664be14fd6eb61454cecd305bc13b90
WenChao Kong <wenchao.kong@gmail.com>
2017-01-08 11:41:52 +1100

    add comments

--- designpattern/src/filterpattern/Demo.kt
+++ designpattern/src/filterpattern/Demo.kt
@@ -4,6 +4,9 @@ import java.util.*
 
 /**
  * Created by wenchaokong on 8/01/2017.
+ *
+过滤器模式允许开发人员使用不同的标准来过滤一组对象，
+通过逻辑运算以解耦的方式把它们连接起来，这种类型的设计模式属于结构型的模式，它可以组合多个标准来获得单一的标准。
  */
 fun main (args : Array<String>){
     val persons = ArrayList<Person>()
