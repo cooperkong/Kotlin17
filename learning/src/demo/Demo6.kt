9e38a9ad869ba7008a8ee4c62d9b805a670d116a
WenChao Kong <wenchao.kong@gmail.com>
2017-01-11 10:43:42 +1100

    lazy delegate, retrofit

---
+++ learning/src/demo/Demo6.kt
@@ -0,0 +1,32 @@
+package demo
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+/**
+ * Created by wenchaokong on 11/01/2017.
+ */
+interface Service{
+    @GET("users/defunkt")
+    fun getUsers() : Call<User>
+
+
+}
+data class User(val login : String, val id : Long)
+
+object GitHubService{
+    val githubService: Service by lazy {
+         Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.github.com")
+                .build().create(Service::class.java)
+    }
+}
+
+
+fun main (args : Array<String>){
+    val github = GitHubService.githubService
+    println(github.getUsers().execute().body())
+
+}
