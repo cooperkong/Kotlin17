4fde2c915fb8cfb548de18e1126269371e1c6f70
WenChao Kong <wenchao.kong@gmail.com>
2017-01-19 10:20:37 +1100

    complete demo

---
+++ learning/src/demo/mediaplayer/Player.kt
@@ -0,0 +1,85 @@
+package demo.mediaplayer
+
+import kotlin.properties.Delegates
+
+/**
+ * Created by wenchaokong on 18/01/2017.
+ */
+class Player{
+    private var state: State by Delegates.observable(State.IDLE, { prop, old, new ->
+        println("$old -> $new")
+        onPlayerStateChangedListener?.onStateChanged(old, new)
+    })
+
+
+    private fun doPlay(url: String, position: Long) {
+        //todo
+    }
+
+    private fun doResume(){
+        //todo
+    }
+
+    private fun doPause() {
+        //todo
+    }
+
+    private fun doStop() {
+        //todo
+    }
+
+    //region api
+    interface OnPlayerStateChangedListener {
+        fun onStateChanged(oldState: State, new: State)
+    }
+
+    var onPlayerStateChangedListener: OnPlayerStateChangedListener? = null
+
+    fun play(url: String, position: Long = 0) {
+        sendCmd(PlayerCmd.Play(url, position))
+    }
+
+    fun resume() {
+        sendCmd(PlayerCmd.Resume)
+    }
+
+    fun pause() {
+        sendCmd(PlayerCmd.Pause)
+    }
+
+    fun stop() {
+        sendCmd(PlayerCmd.Stop)
+    }
+
+    fun seekTo(position: Long) {
+        sendCmd(PlayerCmd.Seek(position))
+    }
+
+    private fun sendCmd(cmd: PlayerCmd) {
+        when (cmd) {
+            is PlayerCmd.Play -> {
+                println("\nPlay ${cmd.url} from ${cmd.position}ms")
+                state = State.PLAYING
+                doPlay(cmd.url, cmd.position)
+            }
+            is PlayerCmd.Resume -> {
+                println("\nResume. ")
+                state = State.PLAYING
+                doResume()
+            }
+            is PlayerCmd.Pause -> {
+                println("\nPause. ")
+                state = State.PAUSED
+                doPause()
+            }
+            is PlayerCmd.Stop -> {
+                println("\nStop.")
+                state = State.IDLE
+                doStop()
+            }
+            is PlayerCmd.Seek -> {
+                println("\nSeek to ${cmd.position}ms, state: $state")
+            }
+        }
+    }
+}
